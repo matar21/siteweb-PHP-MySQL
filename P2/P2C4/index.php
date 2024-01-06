f1f75357d37309cb77b5f86550a696c78a4ef6e4
matar21 <146945270+matar21@users.noreply.github.com>
2024-01-06 19:06:35 +0100

    Update index.php

--- P2/P2C4/index.php
+++ P2/P2C4/index.php
@@ -46,5 +46,40 @@ $recipes = [
         <?php endif; ?>
     <?php endforeach ?>
 </div>
+
+    <h2>Exercice : Ma solution personnelle</h2>
+    <?php
+        $recipes = [
+            [
+                'title' => 'Cassoulet',
+                'recipe' => 'Etape 1 : des flageolets !',
+                'author' => 'mickael.andrieu@exemple.com',
+                'is_enabled' => true,
+            ],
+            [
+                'title' => 'Couscous',
+                'recipe' => 'Etape 1 : de la semoule',
+                'author' => 'mickael.andrieu@exemple.com',
+                'is_enabled' => false,
+            ],
+            [
+                'title' => 'Escalope milanaise',
+                'recipe' => 'Etape 1 : prenez une belle escalope',
+                'author' => 'mathieu.nebra@exemple.com',
+                'is_enabled' => true,
+            ],
+        ];
+        
+        for($i=0; $i<=2; $i++){
+            if(array_key_exists('is_enabled', $recipes[$i]) && $recipes[$i]['is_enabled']=== true){
+                echo "
+                        <h3>{$recipes[$i]['title']}</h3>
+                        <p>{$recipes[$i]['recipe']}</p>
+                        <p>{$recipes[$i]['author']}</p>
+                    ";
+            }
+        }
+    ?>
+    <h2>Veillez juger et repondre svp!</h2>
 </body>
 </html>
